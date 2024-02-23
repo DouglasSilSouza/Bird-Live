0f88fcce9b44e550d93c0fad5cbcb8a1e2c39253
DouglasSilSouza <97581286+DouglasSilSouza@users.noreply.github.com>
2024-02-23 03:34:57 -0300

    modified:   dls_empresa/asgi.py

--- dls_empresa/asgi.py
+++ dls_empresa/asgi.py
@@ -5,7 +5,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
-from ..app_payment.routing import websocket_urlpatterns as app_payment_urlpatterns
+from app_payment.routing import websocket_urlpatterns as app_payment_urlpatterns
 
 websocket_router = URLRouter([
     *app_payment_urlpatterns,
