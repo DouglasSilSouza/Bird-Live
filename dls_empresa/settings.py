048f827034a82d9bf5ffee22df08797a7127dd77
DouglasSilSouza <97581286+DouglasSilSouza@users.noreply.github.com>
2024-03-04 19:56:00 -0300

    modified:   dls_empresa/settings.py

--- dls_empresa/settings.py
+++ dls_empresa/settings.py
@@ -111,7 +111,7 @@ DATABASES={
         "USER": os.getenv("PYUSER"),
         "PASSWORD": os.getenv("PYPASSWORD"),
         "HOST": os.getenv("PYHOST"),
-        "PORT": os.getenv("PYPORT", "3306"),
+        "PORT": "3306",
         "OPTIONS": {
             'sql_mode': 'STRICT_ALL_TABLES',
             'charset': 'utf8mb4',
